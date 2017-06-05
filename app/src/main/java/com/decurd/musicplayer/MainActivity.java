5353e0e5b3f351b3928663e8bc7eeffc4523cdde
decurd <coffee5158@naver.com>
2017-06-05 18:17:01 +0900

    ..

--- app/src/main/java/com/decurd/musicplayer/MainActivity.java
+++ app/src/main/java/com/decurd/musicplayer/MainActivity.java
@@ -1,12 +1,15 @@
 package com.decurd.musicplayer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.Toast;
 
 import com.decurd.musicplayer.fragments.ListViewFragment;
 import com.decurd.musicplayer.fragments.PlayerFragment;
@@ -25,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        PermissionRequester.Builder requester = new PermissionRequester.Builder(this);
+        requester.create().request(Manifest.permission.READ_EXTERNAL_STORAGE, 10000, new PermissionRequester.OnClickDenyButtonListener() {
+            @Override
+            public void onClick(Activity activity) {
+                Toast.makeText(MainActivity.this, "권한을 얻지 못했습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         mPlayerFragment = new PlayerFragment();
         mListViewFragment = new ListViewFragment();
